66ca9da4e023831c98cc20edaee5599df113ca0a
Hakushi <hkshi555@gmail.com>
2025-12-17 15:39:54 +0100

    fix useUser hook

--- uu_ritr_maing01-hi/src/hooks/useUser.js
+++ uu_ritr_maing01-hi/src/hooks/useUser.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useUser() {
   const [user, setUser] = useState(() => JSON.parse(localStorage.getItem("player")));
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("player")));
+  }, [localStorage.getItem("player")]);
 
   return [user, setUser];
 }
